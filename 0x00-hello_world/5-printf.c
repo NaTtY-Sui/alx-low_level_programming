8aaa406c620e9fcbd8d06de5f5209c22cc9e6c08
NaTtY-Sui <nathnaelasfaw5@gmail.com>
2022-07-01 05:52:44 +0000

    Added 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point
+ *
+ * Return: Always 0 (sucess)
+ */
+int main (void)
+{
+	printf("with proper grammer, but the output is a piece of art/n")
+		return 0
+}
+
