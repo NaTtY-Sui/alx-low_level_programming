da4bad9f492427838a46d923e2c1f38889cb0d79
NaTtY-Sui <nathnaelasfaw5@gmail.com>
2022-07-12 18:44:32 +0000

    Added 3-puts.c

--- 0x05-pointers_arrays_strings/main.c
+++ 0x05-pointers_arrays_strings/main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * main - check the code
@@ -8,12 +7,10 @@
  */
 int main(void)
 {
-    char *str;
-    int len;
-
-    str = "My first strlen!";
-    len = _strlen(str);
-    printf("%d\n", len);
-
-    return (0);
+	char *str;
+        
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	_puts(str);
+	
+	return (0);
 }
