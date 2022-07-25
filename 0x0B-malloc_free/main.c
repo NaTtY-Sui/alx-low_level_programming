545944fa60a363fe04457abfbe6e1f552d4b2a21
NaTtY-Sui <nathnaelasfaw5@gmail.com>
2022-07-25 18:53:03 +0000

    Added 2-str_concat.c

--- 0x0B-malloc_free/main.c
+++ 0x0B-malloc_free/main.c
@@ -11,13 +11,13 @@ int main(void)
 {
     char *s;
 
-    s = _strdup("ALX SE");
+    s = str_concat("Betty ", "Holberton");
     if (s == NULL)
     {
-        printf("failed to allocate memory\n");
+        printf("failed\n");
         return (1);
     }
         printf("%s\n", s);
-	free(s);
+        free(s);
         return (0);
 }
