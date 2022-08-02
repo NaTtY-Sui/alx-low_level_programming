e7886d4340663669fdfaed74de50fd38c2729b9e
NaTtY-Sui <nathnaelasfaw5@gmail.com>
2022-08-02 01:45:32 +0000

    Added 1-pi.h

--- 0x0D-preprocessor/main.c
+++ 0x0D-preprocessor/main.c
@@ -1,17 +1,19 @@
-#include "0-object_like_macro.h"
-#include "0-object_like_macro.h"
+#include "1-pi.h"
+#include "1-pi.h"
 #include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
+ *  * main - check the code
+ *   *
+ *    * Return: Always 0.
+ *     */
 int main(void)
 {
-    int s;
+    float a;
+    float r;
 
-    s = 98 + SIZE;
-    printf("%d\n", s);
+    r = 98;
+    a = PI * r * r;
+    printf("%.3f\n", a);
     return (0);
 }
