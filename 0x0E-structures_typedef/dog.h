1cd0e5aa66f7fc1df1c54507a089658b80005fdc
NaTtY-Sui <nathnaelasfaw5@gmail.com>
2022-08-03 06:05:05 +0000

    Added dog_t in dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,14 +8,14 @@
  * @age: age of the dog
  * @owner: owner of the dog
  *
- *
  */
+
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
